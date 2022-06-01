2a790f0100bc1eee2235f38a7236c57adec3597c
pedropaulodf <pedropaulotj@gmail.com>
2022-06-01 15:31:37 -0300

    teste 3

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ setInterval(() => {
 
 let deferredPrompt;
 const addBtn = document.querySelector('.add-button');
-addBtn.style.display = 'none';
+addBtn.style.display = 'block';
 
 window.addEventListener('beforeinstallprompt', (e) => {
   // Prevent Chrome 67 and earlier from automatically showing the prompt
@@ -33,12 +33,11 @@ window.addEventListener('beforeinstallprompt', (e) => {
   // Update UI to notify the user they can add to home screen
   addBtn.style.display = 'block';
 
-  deferredPrompt.prompt();
   addBtn.addEventListener('click', () => {
     // hide our user interface that shows our A2HS button
     addBtn.style.display = 'none';
     // Show the prompt
-    // deferredPrompt.prompt();
+    deferredPrompt.prompt();
     // Wait for the user to respond to the prompt
     deferredPrompt.userChoice.then((choiceResult) => {
       if (choiceResult.outcome === 'accepted') {
