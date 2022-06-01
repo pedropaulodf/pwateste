9d4bf7cdbd1bdfb3eb16d59e732e5bf30881e258
pedropaulodf <pedropaulotj@gmail.com>
2022-06-01 14:47:24 -0300

    fix4

--- index.js
+++ index.js
@@ -13,11 +13,11 @@ setInterval(() => {
 
 // Register service worker to control making site work offline
 
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker
-    .register('/pwateste/sw.js')
-    .then(() => { console.log('Service Worker Registered'); });
-}
+// if ('serviceWorker' in navigator) {
+//   navigator.serviceWorker
+//     .register('/pwateste/sw.js')
+//     .then(() => { console.log('Service Worker Registered'); });
+// }
 
 // Code to handle install prompt on desktop
 
