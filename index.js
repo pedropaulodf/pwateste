db7035b7db3069d7fbb74d00220c8ba3d3fd435e
pedropaulodf <pedropaulotj@gmail.com>
2022-06-01 14:30:43 -0300

    fix

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ setInterval(() => {
 
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker
-    .register('/pwa-examples/a2hs/sw.js')
+    .register('/pwateste/sw.js')
     .then(() => { console.log('Service Worker Registered'); });
 }
 
