6bac6ede1d30293ddb419f55d175e97ce759db91
pedropaulodf <pedropaulotj@gmail.com>
2022-06-01 17:02:42 -0300

    teste

--- script.js
+++ script.js
@@ -2,8 +2,40 @@ const divInstall = document.getElementById('installContainer');
 const butInstall = document.getElementById('butInstall');
 
 /* Put code here */
+window.addEventListener('beforeinstallprompt', (event) => {
+  // Impedir que o mini-infobar apareça no celular.
+  event.preventDefault();
+  console.log('👍', 'beforeinstallprompt', event);
+  // Esconder o evento para que possa ser acionado mais tarde.
+  window.deferredPrompt = event;
+  // Remover a classe 'oculta' do contêiner do botão de instalação.
+  divInstall.classList.toggle('hidden', false);
+});
 
+butInstall.addEventListener('click', async () => {
+  console.log('👍', 'butInstall-clicked');
+  const promptEvent = window.deferredPrompt;
+  if (!promptEvent) {
+    // The deferred prompt isn't available.
+    return;
+  }
+  // Show the install prompt.
+  promptEvent.prompt();
+  // Log the result
+  const result = await promptEvent.userChoice;
+  console.log('👍', 'userChoice', result);
+  // Reset the deferred prompt variable, since
+  // prompt() can only be called once.
+  window.deferredPrompt = null;
+  // Hide the install button.
+  divInstall.classList.toggle('hidden', true);
+});
 
+window.addEventListener('appinstalled', (event) => {
+  console.log('👍', 'appinstalled', event);
+  // Limpa o deferredPrompt para que possa ser coletado como lixo
+  window.deferredPrompt = null;
+});
 
 /* Only register a service worker if it's supported */
 if ('serviceWorker' in navigator) {
