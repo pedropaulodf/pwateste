4e731651e30fc3fa7bcbfa8c8d263dd892086a3d
pedropaulodf <pedropaulotj@gmail.com>
2022-06-01 15:28:21 -0300

    teste 2

--- index.js
+++ index.js
@@ -33,17 +33,19 @@ window.addEventListener('beforeinstallprompt', (e) => {
   // Update UI to notify the user they can add to home screen
   addBtn.style.display = 'block';
 
+  deferredPrompt.prompt();
   addBtn.addEventListener('click', () => {
     // hide our user interface that shows our A2HS button
     addBtn.style.display = 'none';
     // Show the prompt
-    deferredPrompt.prompt();
+    // deferredPrompt.prompt();
     // Wait for the user to respond to the prompt
     deferredPrompt.userChoice.then((choiceResult) => {
       if (choiceResult.outcome === 'accepted') {
         console.log('User accepted the A2HS prompt');
       } else {
         console.log('User dismissed the A2HS prompt');
+        addBtn.style.display = 'block';
       }
       deferredPrompt = null;
     });
